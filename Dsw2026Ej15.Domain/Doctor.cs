7ff67e634eca80ce86917051d37c81dcd40f915a
Eduardo Martinez <nicoeduardo87.9@gmail.com>
2026-06-18 20:02:04 -0300

    Agrego estructura inicial y entidades del ejercicio 15

---
+++ Dsw2026Ej15.Domain/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain;
+
+public class Doctor
+{
+    public String Name { get; init; }
+    public String LicenseNumber { get; init; }
+    public bool IsActive { get; private set; }
+    public Speciality? Speciality { get; private set; }
+
+
+}
+
+
+
---
+++ Dsw2026Ej15/Controllers/DoctorsController.cs
@@ -0,0 +1,29 @@
+﻿using Dsw2026Ej15.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Controllers;
+
+[ApiController]
+[Route("api/doctors")]
+public class DoctorsController : ControllerBase // hereda de controlerbase 
+{
+    public DoctorsController(IPersistence persistence)
+    {
+        persistence = _persistence;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDoctor([FromBody] DoctorModel.Request request)
+    {
+        if(string.IsNullOrWhiteSpace(request.Name) ||
+           string.IsNullOrWhiteSpace(request.LicenseNumber))
+        {
+            return BadRequest("Nombre y Matricula requeridos");
+        }
+
+        var specialty= _persistencia.GetSpecialtyById(request.SpecialityId);
+        return Ok(); //OK representa el code de estado 200
+    }
+     
+}
